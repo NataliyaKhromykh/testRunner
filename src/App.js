dd4cd28a66d90e2648d19fd7e85fb62938513667
Nataliya Khromykh <nataliyakhromykh@Nataliyas-MBP.attlocal.net>
2024-01-10 22:36:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import data from './data';
+import React from 'react';
 import './App.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "#1c2246" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow  className="next"/>,
+    prevArrow: <SamplePrevArrow />
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="BigBox">
+      <div className='smallBox'>
+      <Slider {...settings}>
+        {data.map((data) => (
+          <div key={data.name} className="card">
+            <div className='imgCard'>
+            <img src={data.image} width='200px' alt='food'/>
+            </div>
+
+            <div>
+<p>{data.name}</p>
+<button>read more</button>
+            </div>
+          </div>
+        ))}
+            </Slider>
+      </div>
     </div>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,24 @@
+import img1 from './data/one.JPG';
+import img2 from './data/two.JPG';
+import img3 from './data/three.JPG';
+import img4 from './data/four.JPG';
+
+const data = [
+    {
+        name: "one",
+        image: img1
+    },
+    {
+        name: "two",
+        image: img2
+    },
+    {
+        name: "three",
+        image: img3
+    },
+    {
+        name: "four",
+        image: img4
+    }
+]
+export default data;
