2b306d767e356d6c3e28179b86d2b2fdfa6db2aa
Nataliya Khromykh <nataliyakhromykh@Nataliyas-MBP.attlocal.net>
2024-01-14 13:41:03 -0500

    new commit

--- src/TestContainer.js
+++ src/TestContainer.js
@@ -1,17 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { NewMenuContainerData } from './NewMenuContainer-data';
 import {AiOutlineArrowLeft} from "react-icons/ai";
 import {AiOutlineArrowRight} from "react-icons/ai";
 
 const Carousel = () =>{
     const AllCards = NewMenuContainerData;
+    const slideLength = NewMenuContainerData.length;
 
 
     const [card, setCard] = useState([...AllCards].splice(0,3));
 
+    const autoScroll = true;
+    let slideInterval;
+    let intervalTime = 5000;
+
+
+    const nextSlide = () => {
+        setCard(card === slideLength -1 ? 0 : card + 1);
+    };
+    const prevSlide = () => {
+        setCard(card === 0 ? slideLength -1 : card - 1);
+    };
+    function auto() {
+        slideInterval = setInterval(nextSlide,intervalTime)
+    }
+
+    useEffect(() =>{
+        if (autoScroll) {
+            auto()
+        }
+        return () => clearInterval(slideInterval);
+    },[card])
+
     return (
         <div className="newMenuContainer">
-            <AiOutlineArrowLeft className="menuArrowTwo prev"/>
+            <AiOutlineArrowLeft className="menuArrowTwo prev"  onClick={prevSlide}/>
         {card.map((card) => (
            <div className="newMenuBox">
             <div className="newMenuMiniBox" key={card.index}>
@@ -20,7 +43,7 @@ const Carousel = () =>{
                 </div>
             </div>
         ))}
-        <AiOutlineArrowRight className="menuArrowTwo next"/>
+        <AiOutlineArrowRight className="menuArrowTwo next" onClick={nextSlide}/>
         </div>
 
     
